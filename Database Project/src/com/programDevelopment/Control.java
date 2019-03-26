f6e3ccc3abab1ff25a331d436531bcb3323d4d73
Robert Ichim <robertichim98@gmail.com>
2019-03-26 23:26:15 +0000

    Test to connect
    first attempt at connecting  to local database

--- Database Project/src/com/programDevelopment/Control.java
+++ Database Project/src/com/programDevelopment/Control.java
@@ -1,11 +1,32 @@
 package com.programDevelopment;
 
-public class Control {
+import java.sql.*;
+
+public class Control 
+{
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-
-	}
-
+		try
+		{  
+			//step1 load the driver class  
+			Class.forName("oracle.jdbc.driver.OracleDriver");  
+			  
+			//step2 create  the connection object  
+			Connection con=DriverManager.getConnection(  
+			"jdbc:oracle:thin:@localhost:1521:xe","system","oracle");  
+			  
+			//step3 create the statement object  
+			Statement stmt=con.createStatement();  
+			  
+			con.close();  
+			  
+			}
+			catch(Exception e)
+			{ 
+				System.out.println(e); 
+			}
+		}
 }
+
+
