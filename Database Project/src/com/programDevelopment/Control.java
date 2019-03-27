f74485666f66a4f3b74a9a8d8f5f69f0cd176e01
Robert Ichim <robertichim98@gmail.com>
2019-03-27 11:22:47 +0000

    print test
    print three columns in crime table

--- Database Project/src/com/programDevelopment/Control.java
+++ Database Project/src/com/programDevelopment/Control.java
@@ -14,13 +14,22 @@ public class Control
 			  
 			//step2 create  the connection object  
 			Connection con=DriverManager.getConnection(  
-			"jdbc:oracle:thin:@localhost:1521:xe","system","oracle");  
+			"jdbc:oracle:thin:@localhost:1521:xe","rob","rob");  
 			  
 			//step3 create the statement object  
-			Statement stmt=con.createStatement();  
-			  
+			Statement stmt=con.createStatement(); 
+			
+			ResultSet rs=stmt.executeQuery("select * from CRIMES");  
+			
+			//select the first three columns
+			while(rs.next()) 
+			{
+				System.out.println(rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3) );
+			}
+			
 			con.close();  
 			  
+			
 			}
 			catch(Exception e)
 			{ 
