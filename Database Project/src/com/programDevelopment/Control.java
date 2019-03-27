d03dd3e4c6c2eedaf9bab397a6900c9e75bde1e4
Robert Ichim <robertichim98@gmail.com>
2019-03-27 17:35:24 +0000

    GUI and Database separate
    started working on separate versions for GUI and Database connection

--- Database Project/src/com/programDevelopment/Control.java
+++ Database Project/src/com/programDevelopment/Control.java
@@ -1,41 +1,13 @@
 package com.programDevelopment;
 
-import java.sql.*;
 
 public class Control 
 {
 
 	public static void main(String[] args) 
 	{
-		try
-		{  
-			//step1 load the driver class  
-			Class.forName("oracle.jdbc.driver.OracleDriver");  
-			  
-			//step2 create  the connection object  
-			Connection con=DriverManager.getConnection(  
-			"jdbc:oracle:thin:@localhost:1521:xe","rob","rob");  
-			  
-			//step3 create the statement object  
-			Statement stmt=con.createStatement(); 
-			
-			ResultSet rs=stmt.executeQuery("select * from CRIMES");  
-			
-			//select the first three columns
-			while(rs.next()) 
-			{
-				System.out.println(rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3) );
-			}
-			
-			con.close();  
-			  
-			
-			}
-			catch(Exception e)
-			{ 
-				System.out.println(e); 
-			}
-		}
+		DatabaseConnection connection1=new DatabaseConnection();
+	}
 }
 
 
---
+++ Database Project/src/com/programDevelopment/DatabaseGUI.java
@@ -0,0 +1,80 @@
+package com.programDevelopment;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import java.util.Random;
+
+public class DatabaseGUI extends JFrame implements ActionListener, MouseListener
+{
+	
+	private JButton button1;
+	
+	public DatabaseGUI()
+	{
+		super("Crime Statistics");
+		
+		JPanel Panel1=new JPanel();
+		
+		setLayout(new BorderLayout());
+		
+		add(Panel1,BorderLayout.CENTER);
+		
+		
+	}
+	
+	
+
+	
+	
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
