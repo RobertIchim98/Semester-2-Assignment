7162638dbff84f56f9b9f96f3cf2b41c600ab51d
Robert Ichim <robertichim98@gmail.com>
2019-04-02 00:09:58 +0100

    dropdown menus
    added dropdown menus to gui

--- Database Project/src/com/programDevelopment/DatabaseGUI.java
+++ Database Project/src/com/programDevelopment/DatabaseGUI.java
@@ -11,8 +11,9 @@ public class DatabaseGUI extends JFrame implements ActionListener, MouseListener
 {
 	
 	private JButton button1;
-	public DatabaseConnection connection1;
-	//private DatabaseConnection connection1;
+	private DatabaseConnection connection1;
+	private String[] columnNames= new String[] {"Station", "Divisions",
+            "Dangerous Acts 2004", "Dangerous Acts 2016"};
 	
 	public DatabaseGUI()
 	{	
@@ -22,21 +23,31 @@ public class DatabaseGUI extends JFrame implements ActionListener, MouseListener
 		JFrame Frame=new JFrame("Crime Data Information");
 		JPanel Panel1=new JPanel();
 		button1=new JButton("click to query");
+		
+		JComboBox<String> topics1= new JComboBox<>(columnNames);
+		JComboBox<String> topics2= new JComboBox<>(columnNames);
+		
 		Panel1.setLayout(null);
 		
 		add(Panel1,BorderLayout.CENTER);
 		
 				
 		Panel1.add(button1);
-		button1.setBounds(50,100,100,40);
+		Panel1.add(topics1);
+		Panel1.add(topics2);
+		
+		button1.setBounds(100,450,150,40);
+		topics1.setBounds(100,350,150,40);
+		topics2.setBounds(100,250,150,40);
 		
 		
 		
-		Panel1.setSize(700,700);
-		setSize(700,700);
+		Panel1.setSize(1080,700);
+		setSize(1080,700);
 		setVisible(true);
 		button1.addActionListener(this);
 		
+		//removes the program from memory
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
