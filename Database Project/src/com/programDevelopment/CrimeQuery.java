f9fbd5b90875ed0ac94fff415445e058aa27a782
Robert Ichim <robertichim98@gmail.com>
2019-04-11 22:54:05 +0100

    Top 10 worst
    top ten worst query added and properly implemented into the GUI

--- Database Project/src/com/programDevelopment/CrimeQuery.java
+++ Database Project/src/com/programDevelopment/CrimeQuery.java
@@ -12,7 +12,6 @@ public class CrimeQuery extends JFrame
 {	
 	private String value1;
 	private String value2;
-	private String[] columnsDivisions={"Divisions",value1,value2};
 	JTable table;
 	
 	
@@ -25,19 +24,71 @@ public class CrimeQuery extends JFrame
 		connection1=DatabaseConnection.getConnection();
 	}	
 	 
+	//this will query the whole set required by user 
 	public JTable QueryAll()
 	{	
+		try
+		{
+			String queryStatement="select  Divisions,"+this.getValue1()+","+this.getValue2()+" from CRIMES";
+			PreparedStatement statement=connection1.prepareStatement(queryStatement);
+			table=CreateQueryTable(statement,queryStatement);
+			
+		}
+		catch(Exception e)
+		{
+			JOptionPane.showMessageDialog(this,e);
+		}
+		return table;
+	}
+	
+	
+	//this will sum up the the two topics requested by the user
+	public JTable sumQuery()
+	{	
+		try
+		{
+			String queryStatement="select sum("+this.getValue1()+"),sum("+this.getValue2()+") from CRIMES";
+			PreparedStatement statement=connection1.prepareStatement(queryStatement);
+			table=CreateQueryTable(statement,queryStatement);
+		}
+		catch(Exception e)
+		{
+			JOptionPane.showMessageDialog(this,e);
+		}
+		return table;
+	}
+	public JTable MaxQuery(String value)
+	{
+		try
+		{
+			String queryStatement1="SELECT * FROM \r\n" + 
+								   "    (SELECT station,divisions,"+value+" FROM crimes order by "+value+" desc) \r\n" + 
+								   "where rownum <= 10";
+			PreparedStatement statement=connection1.prepareStatement(queryStatement1);
+			table=CreateQueryTable(statement,queryStatement1);
+		}
+		catch(Exception e)
+		{
+			JOptionPane.showMessageDialog(this,e);
+		}
+		return table;
+	}
+	
+	
+	
+	//this will query the statements needed and will put it in vectors then in a table 
+	private JTable CreateQueryTable(PreparedStatement statement,String queryStatement)
+	{
 		Vector<Object> columnNames=new Vector<Object>();
 		Vector<Object> data=new Vector<Object>();
+		JTable queryTable=new JTable();
 		
 		try
 		{
-			//statement object  
-			Statement stmt=connection1.createStatement();
 			
-			//this will select divisions and what two topics the user wants(value1,value2)
-			ResultSet rs=stmt.executeQuery("select  Divisions,"+this.getValue1()+","+this.getValue2()+" from CRIMES");  
+			ResultSet rs=statement.executeQuery(queryStatement); 
 			
+			//From here onwards this code is from StackOverflow
 			ResultSetMetaData md=rs.getMetaData();
 			
 			int columns=md.getColumnCount();
@@ -61,37 +112,13 @@ public class CrimeQuery extends JFrame
 				data.addElement(row);
 				//System.out.println("row added");
 			}
-			rs.close();
-			stmt.close();
-			connection1.close();
-		}
+		}//here ends code from StackOverflow
 		catch(Exception e)
 		{
-			System.out.println(e);
+			JOptionPane.showMessageDialog(this,e);
 		}
-		table=new JTable(data,columnNames);
-		return table;
-	}
-	
-	
-	
-	public void sumQuery() throws SQLException
-	{
-		try
-		{
-			Statement stmt=connection1.createStatement();
-			PreparedStatement statement=  connection1.prepareStatement("select sum("+this.getValue1()+"),sum("+this.getValue2()+") from CRIMES");
-		    ResultSet rs= statement.executeQuery();
-			
-			while(rs.next())
-			{
-				//System.out.println(rs.getString(this.getValue1())+"||"+rs.getString(this.getValue2()));
-			}
-		}
-		catch(Exception e)
-		{
-			
-		}
+		queryTable=new JTable(data,columnNames);
+		return queryTable;
 	}
 	
 	public String getValue1() {
--- Database Project/src/com/programDevelopment/DatabaseGUI.java
+++ Database Project/src/com/programDevelopment/DatabaseGUI.java
@@ -14,8 +14,9 @@ import java.util.*;
 public class DatabaseGUI extends JFrame implements ActionListener, MouseListener
 {
 	
-	private JButton button1;
-	private DatabaseConnection connection1;
+	
+	
+	//column names which match the column names from the database
 	private String[] columnNames= new String[] {
             "DANGEROUS_ACTS_2004", "DANGEROUS_ACTS_2016",
             "ATTEMPTMURDER_RELATED2004","ATTEMPTMURDER_RELATED2016",
@@ -24,27 +25,86 @@ public class DatabaseGUI extends JFrame implements ActionListener, MouseListener
             "WEAPONS_EXPLOSIVES2004","WEAPONS_EXPLOSIVES2016"};
 	private JComboBox<String> topics1= new JComboBox<>(columnNames);
 	private JComboBox<String> topics2= new JComboBox<>(columnNames);
-	public JPanel Panel1;
-	public JButton button2;
+	private JPanel Panel1;
 	
-	public Vector<String> columns1=new Vector<String>();
-	public Vector<String> data=new Vector<String>();
-	public String value1;
-	public String value2;
-	CrimeQuery query1;
+	private JButton button1;
+	private JButton button2;
+	private JButton button3;
+	private JButton button4;
+	private JButton button5;
+
+	private String value1;
+	private String value2;
+	private CrimeQuery query1;
 	
 	public DatabaseGUI()
 	{	
 		super("Irish Crime Data info");
-		setLayout(new BorderLayout());
 		
-		//JFrame Frame=new JFrame("Crime Data Information");
-		Panel1=new JPanel();
-		JLabel label1=new JLabel("Select two rows to inspect data:");
 		button1=new JButton("Query The selected");
-		//JScrollPane scrolltable = new JScrollPane(table); 
 		button2=new JButton("Sum up rows");
+		button3=new JButton("Select new data");
+		button4=new JButton("<- go back");
+		button5=new JButton("Max rows");
 		
+		
+		setLayout(new BorderLayout());
+	
+		Panel1=new JPanel();
+
+		selectMenu();
+		
+		Panel1.setSize(1080,700);
+		setSize(1080,600);
+		setVisible(true);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+		button4.addActionListener(this);
+		button5.addActionListener(this);
+		
+		//removes the program from memory
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent anything) 
+	{
+		
+		if (anything.getSource() == button1)
+		{	
+			queryAllMenu();
+		}
+		
+		if(anything.getSource() == button2)
+		{
+			querySumMenu();
+		}
+		if(anything.getSource() == button3)
+		{
+			selectMenu();
+		}
+		if(anything.getSource() == button4)
+		{
+			queryAllMenu();
+		}
+		if(anything.getSource() == button5)
+		{
+			queryMaxMenu();
+		}
+		
+		
+		
+	}
+	
+	
+	private void selectMenu()
+	{
+		clearPanel(Panel1);
+		
+		JLabel label1=new JLabel("Select two rows to inspect data:");
 		
 		Panel1.setLayout(null);
 		
@@ -61,57 +121,98 @@ public class DatabaseGUI extends JFrame implements ActionListener, MouseListener
 		topics1.setBounds(100,80,230,60);
 		topics2.setBounds(100,215,230,60);
 
-		label1.setFont(new Font("Serif", Font.CENTER_BASELINE, 24));
-		
-		
-		
-		Panel1.setSize(1080,700);
-		setSize(1080,600);
-		setVisible(true);
-		button1.addActionListener(this);
-		button2.addActionListener(this);
-		
-		//removes the program from memory
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		label1.setFont(new Font("Serif", Font.CENTER_BASELINE, 24));	
 	}
 	
 	
-	@Override
-	public void actionPerformed(ActionEvent anything) 
+	private void queryAllMenu()
 	{
+		clearPanel(Panel1);
 		
-		if (anything.getSource() == button1)
-		{	
-			value1=topics1.getSelectedItem().toString();	
-			value2=topics2.getSelectedItem().toString();
-			
-			query1=new CrimeQuery(value1,value2);
-			//JOptionPane.showMessageDialog(this,table);
-			JTable table=query1.QueryAll();
-			//JOptionPane.showMessageDialog(this,table);
-			JScrollPane scrolltable = new JScrollPane(table);
-			scrolltable.setBounds(500,10,500,500);
-			Panel1.removeAll();
-			Panel1.invalidate();
-			Panel1.validate();
-			Panel1.repaint();
-			Panel1.add(scrolltable);
-			//queryAllTable(table);
-			Panel1.add(button2);
-			button2.setBounds(100,350,230,60);
-		}
-		if(anything.getSource() == button2)
-		{
-			JOptionPane.showMessageDialog(this,"Sum up yo");
-			
-		}
+		setValue1(topics1.getSelectedItem().toString());	
+		setValue2(topics2.getSelectedItem().toString());
+		
+		//load user values into query1
+		setQuery1(new CrimeQuery(getValue1(),getValue2()));
+		
+		//get the table
+		JTable table=getQuery1().QueryAll();
+		
+		//add table with new options
+		JScrollPane scrolltable = new JScrollPane(table);
+		scrolltable.setBounds(500,10,500,500);
+	
+		Panel1.add(scrolltable);
+		//queryAllTable(table);
+		
+		Panel1.add(button2);
+		Panel1.add(button5);
+		Panel1.add(button3);
+		button3.setBounds(260,10,180,60);
+		button2.setBounds(100,350,230,60);
+		button5.setBounds(100,215,230,60);
 	}
-	public void queryAllTable(JTable table)
+	
+	private void querySumMenu()
 	{
+		clearPanel(Panel1);
+		
+		JLabel label1=new JLabel("This is the total numbers\nFor:"+getValue1()+"and"+getValue2());
+		JTable table=getQuery1().sumQuery(); 
+		
+		JScrollPane scrolltable = new JScrollPane(table);
+		label1.setBounds(100,10,400,40);
+		scrolltable.setBounds(500,10,500,500);
+		Panel1.add(scrolltable);
+		Panel1.add(button3);
+		Panel1.add(button4);
+		button3.setBounds(260,10,180,60);
+		button4.setBounds(80,10,180,60);
+	}
+	
+	private void queryMaxMenu()
+	{
+		clearPanel(Panel1);
+		
+		JLabel label1=new JLabel("Top 10 worst from "+this.getValue1()+":");
+		JLabel label2=new JLabel("Top 10 worst from "+this.getValue2()+":");
+		
+		JTable table1=getQuery1().MaxQuery(value1); 
+		JTable table2=getQuery1().MaxQuery(value2); 
+		
+		JScrollPane scrolltable1 = new JScrollPane(table1);
+		JScrollPane scrolltable2= new JScrollPane(table2);
+		
+		scrolltable1.setBounds(500,60,500,190);
+		scrolltable2.setBounds(500,310,500,190);
+		button3.setBounds(260,10,180,60);
+		button4.setBounds(80,10,180,60);
+		label1.setBounds(100,10,500,190);
+		label2.setBounds(100,280,500,190);
+		
+		label1.setFont(new Font("Serif", Font.CENTER_BASELINE, 15));
+		label2.setFont(new Font("Serif", Font.CENTER_BASELINE, 15));
+		
+		Panel1.add(scrolltable1);
+		Panel1.add(scrolltable2);
+		Panel1.add(button3);
+		Panel1.add(button4);
+		Panel1.add(label1);
+		Panel1.add(label2);
+		
 		
 	}
 	
+	
+	
+	//this will clear the panel so there wont be need for more
+	private void clearPanel(JPanel Panel1)
+	{
+		Panel1.removeAll();
+		Panel1.invalidate();
+		Panel1.validate();
+		Panel1.repaint();
+	}
 	
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
@@ -141,6 +242,36 @@ public class DatabaseGUI extends JFrame implements ActionListener, MouseListener
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		
+	}
+
+
+	public String getValue1() {
+		return value1;
+	}
+
+
+	public void setValue1(String value1) {
+		this.value1 = value1;
+	}
+
+
+	public String getValue2() {
+		return value2;
+	}
+
+
+	public void setValue2(String value2) {
+		this.value2 = value2;
+	}
+
+
+	public CrimeQuery getQuery1() {
+		return query1;
+	}
+
+
+	public void setQuery1(CrimeQuery query1) {
+		this.query1 = query1;
 	}
 
 
